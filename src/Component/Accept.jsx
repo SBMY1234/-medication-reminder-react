9504bd1cafb57e84ab989acc76e18734cb65f00e
sasab1234 <5319sb@gmail.com>
2022-05-20 00:12:37 +0300

    add dialog

--- src/Component/Accept.jsx
+++ src/Component/Accept.jsx
@@ -5,67 +5,82 @@ export const Accept = () => {
     return <>
 
 
-<div id="part2">
-<div id="btn">
-  <button type="button" className="btn">
-    1
-  </button>
-  <p className="pratim" id="details">
-    {" "}
-    פרטים אישיים
-  </p>
-</div>
-<div className="line">-------</div>
-<div id="btn2">
-  <button type="button" className="btn2">
-    2
-  </button>
-  <p className="pratim" id="list">
-    {" "}
-    רשימת תרופות
-  </p>
-</div>
-<div className="line" id="line2">
-  -------
-</div>
-<div id="btn3">
-  <button type="button" className="btn3">
-    3
-  </button>
-  <p className="pratim" id="okey">
-    {" "}
-    אישור ושליחה
-  </p>
-</div>
-</div>
-<div id="part3">
-<h1>אישור</h1>
-<br />
-<br />
-<form className="check">
-  <input
-    type="checkbox"
-    id="vehicle3"
-    name="vehicle3"
-    defaultValue="Boat"
-  />
-  <label htmlFor="vehicle3">
-    הרייני מאשר/ת קבלת התראות למספר הפלאפון:0555555555
-  </label>
-  <br />
-  <br />
-  <br />
-  <button type="submit" className="elementor-button elementor-size-md">
-    <span>
-      <span className=" elementor-button-icon">
-        <i className="fa fa-location-arrow" aria-hidden="true" />{" "}
-      </span>
-      <span className="elementor-button-text">שליחה</span>
-    </span>
-  </button>
-</form>
+        <div id="part2">
+            <div id="btn">
+                <button type="button" className="btn">
+                    1
+                </button>
+                <p className="pratim" id="details">
+                    {" "}
+                    פרטים אישיים
+                </p>
+            </div>
+            <div className="line">-------</div>
+            <div id="btn2">
+                <button type="button" className="btn2">
+                    2
+                </button>
+                <p className="pratim" id="list">
+                    {" "}
+                    רשימת תרופות
+                </p>
+            </div>
+            <div className="line" id="line2">
+                -------
+            </div>
+            <div id="btn3">
+                <button type="button" className="btn3">
+                    3
+                </button>
+                <p className="pratim" id="okey">
+                    {" "}
+                    אישור ושליחה
+                </p>
+            </div>
+        </div>
+        <div id="part3">
+            <h1>אישור</h1>
+            <br />
+            <br />
+            <form className="check">
+                <input
+                    type="checkbox"
+                    id="vehicle3"
+                    name="vehicle3"
+                    defaultValue="Boat"
+                />
+                <label htmlFor="vehicle3">
+                    הרייני מאשר/ת קבלת התראות למספר הפלאפון:0555555555
+                </label>
+                <br />
+                <br />
+                <br />
+                <button type="submit" className="elementor-button elementor-size-md">
+                    <span>
+                        <span className=" elementor-button-icon">
+                            <i className="fa fa-location-arrow" aria-hidden="true" />{" "}
+                        </span>
+                        <span className="elementor-button-text">שליחה</span>
+                    </span>
+                </button>
+            </form>
 
-</div>
-</>
+        </div>
+
+
+        <div id="part3">
+            <h1>הנתונים נשמרו בהצלחה</h1>
+            <br />
+            <img
+                className="image"
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0RKXXuUEjR-T_o96VZHkQYOvbhDNI5pLmhO7JmdRm6IWcbCdr_RMSqaQxsceBhnSrWb8&usqp=CAU"
+            />
+            <br />
+            <br />
+            <p className="by">מהיום התזכורות עלינו</p>
+            <p className="by">רק בריאות!</p>
+            <h4>תרופון</h4>
+        </div>
+    </>
 }
 export default Accept;
--- src/Component/ListMedicals.jsx
+++ src/Component/ListMedicals.jsx
@@ -1,75 +1,89 @@
-
+import Dialog from './Dialog'
 import './ListMedicals.css';
+
 export const ListMedicals = () => {
 
-
-
-
-    return <>
-<div id="part2">
-    <div id="btn">
-      <button type="button" className="btn">
-        1
-      </button>
-      <p className="pratim" id="details">
-        {" "}
-        פרטים אישיים
-      </p>
+  return <>
+    <div id="part2">
+      <div id="btn">
+        <button type="button" className="btn">
+          1
+        </button>
+        <p className="pratim" id="details">
+          {"פרטים אישיים "}
+          
+        </p>
+      </div>
+      <div className="line">-------</div>
+      <div id="btn2">
+        <button type="button" className="btn2">
+          2
+        </button>
+        <p className="pratim" id="list">
+          {"רשימת תרופות "}
+          
+        </p>
+      </div>
+      <div className="line" id="line2">
+        -------
+      </div>
+      <div id="btn3">
+        <button type="button" className="btn3">
+          3
+        </button>
+        <p className="pratim" id="okey">
+          {" אישור ושליחה"}
+          
+        </p>
+      </div>
     </div>
-    <div className="line">-------</div>
-    <div id="btn2">
-      <button type="button" className="btn2">
-        2
-      </button>
-      <p className="pratim" id="list">
-        {" "}
-        רשימת תרופות
-      </p>
+    <div id="part3">
+      <h1>רשימת התרופות של שמואל שמואלי:</h1>
+      <canvas id="myCanvas" className="rec"></canvas>
+      {/* <button type="button" className="btn4">
+        הוסף תרופה
+      </button> */}
+      <Dialog />
+      <div className="box">
+        <h1 className="acamol">אקמול</h1>
+        <h1 className="date">תאריך עדכון אחרון:</h1>
+        <h1 className="date2">15/05/2022</h1>
+        <button type="button" className="btn5">
+          עריכה
+        </button>
+      </div>
+      <canvas className="rec2"></canvas>
+      <div className="box">
+        <h1 className="acamol" id="decsamol">
+          דקסמול
+        </h1>
+        <h1 className="date" id="lastDate">
+          תאריך עדכון אחרון:
+        </h1>
+        <h1 className="date2" id="lastDate2">
+          15/05/2022
+        </h1>
+        <button type="button" className="btn5" id="update">
+          עריכה
+        </button>
+      </div>
+      <canvas className="rec2" id="reco"></canvas>
+      <div className="box">
+        <h1 className="acamol" id="decsamol2">
+          אופטלגין
+        </h1>
+        <h1 className="date" id="lastDate3">
+          תאריך עדכון אחרון:
+        </h1>
+        <h1 className="date2" id="lastDate4">
+          05/08/2021
+        </h1>
+        <button type="button" className="btn5" id="update2">
+          עריכה
+        </button>
+      </div>
+      <canvas className="rec2" id="reco2"></canvas>
     </div>
-    <div className="line" id="line2">
-      -------
-    </div>
-    <div id="btn3">
-      <button type="button" className="btn3">
-        3
-      </button>
-      <p className="pratim" id="okey">
-        {" "}
-        אישור ושליחה
-      </p>
-    </div>
-  </div>
-  <div id="part3">
-    <h1>רשימת התרופות של ישראל ישראלי:</h1>
-    <canvas id="myCanvas" className="rec"></canvas>
-    <button type="button" className="btn4">
-      הוסף תרופה
-    </button>
-    <div className="box">
-      <h1 className="acamol">אקמול</h1>
-      <h1 className="date">תאריך עדכון אחרון:</h1>
-      <h1 className="date2">15/05/2022</h1>
-      <button type="button" className="btn5">
-        עריכה
-      </button>
-    </div>
-    <canvas className="rec2"></canvas>
-    <div className="box">
-      <h1 className="acamol" id="decsamol">
-        דקסמול
-      </h1>
-      <h1 className="date" id="lastDate">
-        תאריך עדכון אחרון:
-      </h1>
-      <h1 className="date2" id="lastDate2">
-        15/05/2022
-      </h1>
-      <button type="button" className="btn5" id="update">
-        עריכה
-      </button>
-    </div>
-    <canvas className="rec2" id="reco"></canvas>
-  </div>
 
   </>
 
